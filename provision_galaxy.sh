384e2ebe420963ab0ca501f888ffb7363459ed80
Michael Bright <mjbright@github.com>
2015-08-05 13:52:00 +0200

    Modified to use git or mercurial

--- provision_galaxy.sh
+++ provision_galaxy.sh
@@ -1,21 +1,48 @@
-# install prereqs
-echo "installing mercurial..."
-sudo apt-get -y install mercurial mercurial-server
 
-# install galaxy
-echo "installing galaxy..."
+HG_URL=https://bitbucket.org/galaxy/galaxy-dist/
+GIT_URL=https://github.com/galaxyproject/galaxy/
+
+USE_GIT=1
+
 export GALAXYPATH="/vagrant/galaxy-dist/"
-hg clone https://bitbucket.org/galaxy/galaxy-dist/ $GALAXYPATH
-cd $GALAXYPATH
 
-echo "updating to stable..."
-hg update stable
+if [ $USE_GIT -ne 0 ];then
+    # install prereqs
+    echo "installing git..."
+    sudo apt-get -y install git
 
-# configure galaxy
-echo "configuring galaxy..."
-# copy default configurations over
-cp /vagrant/config/* $GALAXYPATH/
+    # install galaxy
+    echo "installing galaxy..."
+    git clone $GIT_URL $GALAXYPATH
+
+    cd $GALAXYPATH
+    echo "updating to stable..."
+    git checkout -b master origin/master
+
+else
+    # install prereqs
+    echo "installing mercurial..."
+    sudo apt-get -y install mercurial mercurial-server
+
+    # install galaxy
+    echo "installing galaxy..."
+    hg clone $HG_URL $GALAXYPATH
+
+    cd $GALAXYPATH
+    echo "updating to stable..."
+    hg update stable
+
+    # configure galaxy
+    #echo "configuring galaxy..."
+    # copy default configurations over
+    #cp /vagrant/config/* $GALAXYPATH/
+fi
 
 # start galaxy
-echo "running galaxy daemon..."
-sh run.sh --daemon
+echo "running galaxy ..."
+sh run.sh
+
+#echo "running galaxy daemon..."
+#sh run.sh --daemon
+
+
